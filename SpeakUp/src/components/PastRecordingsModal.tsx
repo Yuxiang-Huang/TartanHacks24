41fec16f464c019d2f282140bfe8c989fb26cf47
acee3 <alexjakhangcheung@gmail.com>
2024-02-03 00:02:32 -0500

    some css changes

--- SpeakUp/src/components/PastRecordingsModal.tsx
+++ SpeakUp/src/components/PastRecordingsModal.tsx
@@ -14,22 +14,27 @@ const PastRecordingsModal: React.FC<PastRecordingsModalProps> = ({ isOpen, onClo
     useEffect(() => {
         // call thing to get past data
 
-        setPastRecordings([[{ text: "Hello my name ", rating: 0.93 }, { text: "is Alex", rating: 0.23 }], [{ text: "Hello my ", rating: 0.11 }, { text: "name is Tor ", rating: 0.48 }], [{ text: "Hello ", rating: 0.53 }, { text: "my name is Yuxiang", rating: 0.98 }]])
+        setPastRecordings([[{ text: "Hello my name ", rating: 0.93 }, { text: "is Alex", rating: 0.23 }], [{ text: "Hello my ", rating: 0.11 }, { text: "name is Tor ", rating: 0.48 }], [{ text: "Hello ", rating: 0.53 }, { text: "my name is Yuxiang", rating: 0.98 }],[{ text: "Hello my name ", rating: 0.93 }, { text: "is Alex", rating: 0.23 }], [{ text: "Hello my ", rating: 0.11 }, { text: "name is Tor ", rating: 0.48 }], [{ text: "Hello ", rating: 0.53 }, { text: "my name is Yuxiang", rating: 0.98 }]])
     }, []);
 
 
     return (
-        <div className={showHideClassName}>
-            <button onClick={onClose}>
-                Close
-            </button>
-            <ul>
-                {pastRecordings.map((pastRecording, index) => (
-                    <li key={index}>
-                        <TextBox textSegments={pastRecording} />
-                    </li>
-                ))}
-            </ul>
+        <div className={showHideClassName + "fixed w-full h-full bg-black/50"}>
+            <div className="rounded-xl py-5 px-10 bg-gradient-to-br from-red-400 to-red-600 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-h-96 overflow-y-scroll">
+                <button className="sticky top-0 float-end -me-5 rounded-xl py-1 px-3 bg-red-300" onClick={onClose}>
+                    X
+                </button>
+                <ul>
+                    {pastRecordings.map((pastRecording, index) => (
+                        <li key={index} className="bg-white rounded-md my-3 p-3 shadow-[inset_0_0_10px_2px_rgba(0,0,0,0.5)]">
+                            <h2>Past Recording #{index}</h2>
+                            <audio></audio>
+                            <button>Rescore</button>
+                            {/* <TextBox textSegments={pastRecording} /> */}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
--- SpeakUp/tailwind.config.js
+++ SpeakUp/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        normal: ['Roboto', 'sans-serif'],
+        special: ['Playfair Display', 'serif']
+      }
+    },
   },
   plugins: [],
 };

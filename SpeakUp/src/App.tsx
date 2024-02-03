d877c0aece60ee02dd801e6453097f3e3f7da2b3
acee3 <alexjakhangcheung@gmail.com>
2024-02-02 19:55:06 -0500

    deleted some boiler

--- SpeakUp/src/App.tsx
+++ SpeakUp/src/App.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "./css/global.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link italic"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <h1>SpeakUP</h1>
+      <svg height="24em" className="mx-auto w-1/2 min-w-100 min-h-24" preserveAspectRatio="none">
+        <defs>
+          <linearGradient id="gradient" gradientTransform="rotate(45)">
+            <stop offset="5%" stop-color="pink" />
+            <stop offset="95%" stop-color="red" />
+          </linearGradient>
+        </defs>
+        <circle r="150" cx="50%" cy="50%" fill="url(#gradient)" />
+
+      </svg>
     </div>
   );
 }
